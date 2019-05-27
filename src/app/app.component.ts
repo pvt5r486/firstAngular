4dc4c4260cda69f78082ee0a39ed808d924c8c85
pvt5r486 <pvt5r486@gmail.com>
2019-05-27 21:14:44 +0800

    使用雙向繫結

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,13 @@ export class AppComponent {
   title = 'firstAngular';
   link = 'http://www.google.com';
   imgUrl = '/assets/images/logo.png';
-  inputStrLen = 0;
+  inputValue = '';
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title = 'changeTitle';
     }
   }
-  calcLength(str: string) {
-    this.inputStrLen = str.length;
-  }
-  cleanInput(inputEl: HTMLInputElement) {
-    this.inputStrLen = 0;
-    inputEl.value = '';
+  cleanInput() {
+    this.inputValue = '';
   }
 }
