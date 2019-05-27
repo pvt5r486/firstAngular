ee04364ccb7edb776e3204f4bfd7af8e0f291527
pvt5r486 <pvt5r486@gmail.com>
2019-05-27 20:08:43 +0800

    完成第一階段的資料繫結實作練習

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstAngular';
+  link = 'http://www.google.com';
+  imgUrl = '/assets/images/logo.png';
+  inputStrLen = 0;
+  changeTitle(altKey: boolean) {
+    if (altKey) {
+      this.title = 'changeTitle';
+    }
+  }
+  calcLength(str: string) {
+    this.inputStrLen = str.length;
+  }
+  cleanInput(inputEl: HTMLInputElement) {
+    this.inputStrLen = 0;
+    inputEl.value = '';
+  }
 }
