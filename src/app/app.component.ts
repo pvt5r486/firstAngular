fdf6565eb65a0fcd81ff3c7a5c4d8615a89aacf5
pvt5r486 <pvt5r486@gmail.com>
2019-05-29 20:33:06 +0800

    安全導覽運算子

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,10 @@ export class AppComponent {
     {
       "id": 1,
       "href": "http://blog.miniasp.com/post/2016/04/30/Visual-Studio-Code-from-Command-Prompt-notes.aspx",
-      "title": "從命令提示字元中開啟 Visual Studio Code 如何避免顯示惱人的偵錯訊息",
+      "subject": {
+        "title": "從命令提示字元中開啟 Visual Studio Code 如何避免顯示惱人的偵錯訊息",
+        "subtite": "Visual Studio Code"
+      },
       "date": "2016/04/30 18:05",
       "author": "GHJKL",
       "category": "Visual Studio",
@@ -22,7 +25,6 @@ export class AppComponent {
     {
       "id": 2,
       "href": "http://blog.miniasp.com/post/2016/03/22/Does-Certification-Exam-Useful.aspx",
-      "title": "考證照真的沒用嗎？一個從業 20 年的 IT 主管告訴你他怎麼看！",
       "date": "2016/03/22 19:28",
       "author": "GHJKL",
       "category": "心得分享",
@@ -32,7 +34,10 @@ export class AppComponent {
     {
       "id": 3,
       "href": "http://blog.miniasp.com/post/2016/03/14/ASPNET-MVC-Developer-Note-Part-28-Understanding-ModelState.aspx",
-      "title": "ASP.NET MVC 開發心得分享 (28)：深入瞭解 ModelState 內部細節",
+      "subject": {
+        "title": "ASP.NET MVC 開發心得分享 (28)：深入瞭解 ModelState 內部細節",
+        "subtite": ""
+      },
       "date": "2016/03/14 12:14",
       "author": "GHJKL",
       "category": "ASP.NET MVC",
@@ -42,7 +47,10 @@ export class AppComponent {
     {
       "id": 4,
       "href": "http://blog.miniasp.com/post/2016/03/06/ASPNET-MVC-5-View-Roslyn-problem-workaround.aspx",
-      "title": "ASP.NET MVC 5.2.3 的 View 使用 Roslyn (C# 6.0) 編譯時的問題",
+      "subject": {
+        "title": "ASP.NET MVC 5.2.3 的 View 使用 Roslyn (C# 6.0) 編譯時的問題",
+        "subtite": ""
+      },
       "date": "2016/03/06 17:11",
       "author": "GHJKL",
       "category": "ASP.NET MVC",
@@ -52,7 +60,10 @@ export class AppComponent {
     {
       "id": 5,
       "href": "http://blog.miniasp.com/post/2016/02/19/Useful-tool-PackageManagement-OneGet.aspx",
-      "title": "介紹好用工具：Win 10 內建的 PackageManagement 套件管理器 (OneGet)",
+      "subject": {
+        "title": "介紹好用工具：Win 10 內建的 PackageManagement 套件管理器 (OneGet)",
+        "subtite": ""
+      },
       "date": "2016/02/19 11:55",
       "author": "GHJKL",
       "category": "介紹好用工具",
@@ -62,7 +73,10 @@ export class AppComponent {
     {
       "id": 6,
       "href": "http://blog.miniasp.com/post/2016/02/02/JavaScript-novice-advice-and-learning-resources.aspx",
-      "title": "我要成為前端工程師！給 JavaScript 新手的建議與學習資源整理",
+      "subject": {
+        "title": "我要成為前端工程師！給 JavaScript 新手的建議與學習資源整理",
+        "subtite": ""
+      },
       "date": "2016/02/02 17:48",
       "author": "GHJKL",
       "category": "前端工程研究",
