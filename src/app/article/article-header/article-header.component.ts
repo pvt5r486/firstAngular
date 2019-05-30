ea5cbac9a92d62cc0633319c3f53eb50e1fa11dd
pvt5r486 <pvt5r486@gmail.com>
2019-05-30 20:28:00 +0800

    拆分父子元件並向下傳遞

---
+++ src/app/article/article-header/article-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-article-header',
+  templateUrl: './article-header.component.html',
+  styleUrls: ['./article-header.component.scss']
+})
+export class ArticleHeaderComponent implements OnInit {
+  @Input()
+  item;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
