8e37f53dc05d92ef94cc3c45c8204b3bbdb7bc3f
pvt5r486 <pvt5r486@gmail.com>
2019-06-02 16:43:59 +0800

    介紹 Async 管道元件

--- src/app/article/article-list/article-list.component.ts
+++ src/app/article/article-list/article-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-article-list',
@@ -8,34 +9,10 @@ import { DataService } from '../data.service';
 })
 export class ArticleListComponent implements OnInit {
   counter = 0;
-  atticleData;
+  atticleData$: Observable<any>;
   constructor(public datasvc: DataService) {
   }
   ngOnInit() {
-    this.datasvc.getData().subscribe((result) => {
-      this.atticleData = result;
-    });
-  }
-
-  doDelete(item) {
-    this.datasvc.doDelete(item).subscribe((result) => {
-      this.atticleData = this.atticleData.filter(v => v.id !== item.id);
-    },
-    (error) => {
-      console.log('錯誤資訊', error);
-    });
-  }
-  doModify(post: any) {
-    this.datasvc.doModify(post).subscribe((result) => {
-      this.atticleData = this.atticleData.map((item) => {
-        if (post.id === item.id) {
-          return Object.assign({}, item, post);
-        }
-        return item;
-      });
-    },
-    (error) => {
-      console.log('錯誤資訊', error);
-    });
+    this.atticleData$ = this.datasvc.getData();
   }
 }
